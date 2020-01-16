7141e39db2ac577401e8eed11404cdc247ca1f6b
96wilkinson <96wilkinson@sky.com>
2020-01-16 14:08:07 +0000

    ready for deployment, application.properties fixed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO Climber (username,firstname,surname,id) VALUES
+    ('Scrappy','John', 'Gordon',1),
+    ('96wilkinson','Anthony','Wilkinson',2),
+    ('jh447','Jordan','Harry',3);
+
+
+INSERT INTO Climbing_Attempts (day,month,year,difficulty,time_spent,climber_id) VALUES
+    (18,12,1996,3,5,1),
+    (22,11,1997,3,5,1),
+    (18,9,1976,3,5,2);
