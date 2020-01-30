5f78948074e0105f1a7cbbef823f2acb79d6ee53
96wilkinson <96wilkinson@sky.com>
2020-01-30 16:48:36 +0000

    pom changed to allow Nexus

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 
-INSERT INTO Climber (username,firstname,surname) VALUES
+INSERT INTO climber (username,firstname,surname) VALUES
     ('Scrappy','John', 'Gordon'),
     ('96wilkinson','Anthony','Wilkinson'),
     ('jh447','Jordan','Harry');
 
 
-INSERT INTO Climbing_Attempts (day,month,year,difficulty,time_spent) VALUES
+INSERT INTO climbing_attempts (day,month,year,difficulty,time_spent) VALUES
     (18,12,1996,3,5),
     (22,11,1997,3,5),
     (18,9,1976,3,5);
