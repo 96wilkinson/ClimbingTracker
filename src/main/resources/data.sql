a24eeb942f5f7efcce0abb73a49ae7da85ac09aa
96wilkinson <58259270+96wilkinson@users.noreply.github.com>
2020-02-06 19:37:32 +0000

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,3 @@
-#CREATE TABLE climber 
-#(ID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-#username VARCHAR(255),
-#firstname VARCHAR(255),
-#surname VARCHAR(255));
-
-#CREATE TABLE climbing_attempts
-#(ID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-#day INT(2),
-#month INT(2),
-#year INT(4),
-#difficulty INT(1),
-#time_spent INT(2));
 
 INSERT INTO climber (username,firstname,surname) VALUES
     ('Scrappy','John', 'Gordon'),
